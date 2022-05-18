e1a3356f4afa065c0c720faa8786fa67c4aac6d0
irakli qiria <irakliqiria8@gmail.com>
2022-05-18 12:43:39 +0400

    correction

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\softDeletes;
 
 class Post extends Model
 {
-    use HasFactory, softDeletes;
+    // use HasFactory, softDeletes;
+    use HasFactory;
 
     public function comments()
     {
